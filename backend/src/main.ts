1c7f8c4375fedbf306fcc47cc04c36ee0d003029
fahad vm <fahadfad444@gmail.com>
2026-01-30 17:51:10 +0530

    fix:[backend]-Cors handled with vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,10 @@ async function bootstrap(): Promise<void> {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ||'http://localhost:5173',
+    origin: [
+      'http://localhost:3000',
+      'https://snip-seven.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'X-Requested-With'],
